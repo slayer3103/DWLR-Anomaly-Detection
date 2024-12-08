8eb75178612d7339904e32e2c6a6a8ad6a340551
slayer3103 <136792566+slayer3103@users.noreply.github.com>
2024-12-08 21:08:32 +0530

    Update prediction.py

--- Anomaly_Detection_In_DWLR/backend/prediction.py
+++ Anomaly_Detection_In_DWLR/backend/prediction.py
@@ -16,7 +16,7 @@ import time
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Define directories relative to BASE_DIR
-MODEL_DIR = os.path.join(BASE_DIR, '../trained_models')  # Models in the parent folder of backend
+MODEL_DIR = os.path.join(BASE_DIR, '/trained_models')  
 UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
 OUTPUT_FOLDER = os.path.join(BASE_DIR, 'output')
 
